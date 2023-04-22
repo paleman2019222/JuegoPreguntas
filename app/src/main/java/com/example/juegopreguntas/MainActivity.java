370c2e5dd78faf1a4f7732fecb2f5902a63a837c
Pablo Javier Alemán Velásquez <paleman-2019222@kinal.edu.gt>
2023-04-21 19:56:03 -0600

    Creación del proyecto

---
+++ app/src/main/java/com/example/juegopreguntas/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.juegopreguntas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
